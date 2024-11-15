ac4d81b2391780c2a8baba1864c69985a46dda07
AllanOLVS <allan.oliveiraa009@gmail.com>
2024-11-15 03:20:47 +0000

    Questão2

---
+++ Questao2.java
@@ -0,0 +1,20 @@
+public class Questao2 {
+    
+    public static void main(String[] args ) throws Exception {
+            
+    int resultadoSoma = 0;
+            
+        for(int i = 1; i <= 1000 ;i++) {
+                
+            resultadoSoma += i;
+                
+        }
+    
+        System.out.printf("Soma = %d%n", resultadoSoma);
+            
+    }
+    
+}
+
+
+
