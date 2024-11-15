6867a8b5603d597ac35236872343f10a7a4c9324
AllanOLVS <allan.oliveiraa009@gmail.com>
2024-11-15 03:18:07 +0000

    Questão1

---
+++ Questao1.java
@@ -0,0 +1,19 @@
+
+
+public class Questao1 {
+
+
+	public static void main(String[] args ) throws Exception {
+		
+		for (int i = 150; i <= 300; i++) {
+			
+			System.out.println(i);
+			
+			
+		}
+			
+			
+	}
+		
+
+}
